7ed71297e51d80808c60830f02050e18cecf1114
Arthur Silva <root.arthur@gmail.com>
2021-02-28 09:10:42 -0800

    chore: create baseUrl, tokenKey and layout constants

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'http://localhost:3001',
+  lsToken: 'LS-TOKEN',
+  layout: {
+    sidebar: true,
+    loading: false
+  },
 };
 
 /*
