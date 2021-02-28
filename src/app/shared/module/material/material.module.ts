95637705221c90931ed20a71254efb74fe354ad5
Arthur Silva <root.arthur@gmail.com>
2021-02-28 09:08:53 -0800

    chore: add angula materia design

---
+++ src/app/shared/module/material/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core'
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class MaterialModule { }
